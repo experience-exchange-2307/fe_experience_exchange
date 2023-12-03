132c9d069f3ace4a46d08b0cf1d18babb973bab1
Jen Ziel <jenziel400@gmail.com>
2023-12-03 15:41:33 -0500

    Add basic styling for the profile container.

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,16 +1,52 @@
-import "./Profile.css"
+import "./Profile.css";
 import React from "react";
 import { CurrentUser } from "types";
-import frog from "../../images/Frog.png"
+import frog from "../../images/Frog.png";
 interface ProfileProps {
   currentUser: CurrentUser | undefined;
 }
 
 function Profile({ currentUser }: ProfileProps) {
   return (
-    <div className="profile-container">
-      <p>{currentUser?.attributes.first_name}</p>
-      <img src={frog} alt='user icon'></img>
+    <div className='profile-page'>
+      <div className='profile-banner'></div>
+      <div className='profile-container'>
+        <div className='profile-container-upper'>
+          <div className='profile-icon-container'>
+            <img src={frog} alt='user icon' className='profile-icon'></img>
+          </div>
+          <p className='profile-name'>
+            {currentUser?.attributes.first_name}{" "}
+            {currentUser?.attributes.last_name}
+          </p>
+        </div>
+        <div className='profile-lower-alignment'>
+          <div className='profile-container-lower'>
+            <div className='profile-lower-left'>
+              <p className='profile-header'>About me</p>
+              <p>{currentUser?.attributes.about}</p>
+            </div>
+            <div className='profile-lower-right'>
+              <article className="profile-list">
+                <p className='profile-header profile-skill-container'>Skills</p>
+                {currentUser?.attributes.skills.map((skill, index) => (
+                  <p className='profile-skill' key={index}>
+                    {skill.name}
+                  </p>
+                ))}
+              </article>
+              <article className="profile-list">
+                <p className='profile-header'>Location</p>
+                <p>{currentUser?.attributes.address}</p>
+              </article>
+              <article className="profile-list">
+                <p className='profile-header'>Email</p>
+                <p>{currentUser?.attributes.email}</p>
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
