66d33562756d6b04e801c149d4be76e87472d19d
Marisa Wyatt <risawyatt22@gmail.com>
2023-12-04 11:39:00 -0700

    feat: Write structure for meetingcards component

--- src/Components/MeetingsContainer/MeetingsContainer.tsx
+++ src/Components/MeetingsContainer/MeetingsContainer.tsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
+import "./MeetingsContainer.css";
+import MeetingCards from "Components/MeetingCards/MeetingCards";
+import { CurrentUser } from "types";
 
-function MeetingsContainer() {
-  return (
-    <div>
-      
-    </div>
-  )
+interface MeetingProps {
+  currentUser: CurrentUser | undefined;
 }
 
-export default MeetingsContainer
+const meetings ={
+  "data": [
+    {
+      "id": "23",
+      "type": "meeting",
+      "attributes": {
+        "partner_id": "2",
+        "date": "01-01-2023",
+        "start_time": "07:30",
+        "end_time": "08:30",
+        "is_host": "true"
+      }
+    },
+    {
+      "id": "27",
+      "type": "meeting",
+      "attributes": {
+        "partner_id": "3",
+        "date": "01-05-2023",
+        "start_time": "11:00",
+        "end_time": "11:30",
+        "is_host": "false"
+      }
+    }
+  ]
+}
+
+// function MeetingsContainer({ currentUser }: MeetingProps) {
+//   const meetings = currentUser?.attributes.meetings || [];
+
+//   return (
+//     <div className="meetings-container">
+//       {!!meetings.length ? (
+//         <MeetingCards meetings={meetings} />
+//       ) : (
+//         <h2>No meetings yet, add meetings!</h2>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default MeetingsContainer;
