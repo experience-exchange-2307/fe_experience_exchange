f8a4d2d185fede33fc672430e9a8cca27a2bece9
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-12-04 20:26:07 -0500

    Add createNewUser to handle post

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,13 +1,14 @@
 import Profile from 'Components/Profile/Profile';
 import './App.css';
 import { useEffect, useState } from 'react';
-import { CurrentUser } from 'types';
-import { getSingleUser } from 'apiCalls';
+import { CurrentUser, NewUserData } from 'types';
+import { getSingleUser, postNewUser } from 'apiCalls';
 import {Routes, Route} from "react-router-dom"
 import Nav from 'Components/Nav/Nav';
 import ErrorPage from 'Components/ErrorPage/ErrorPage';
 import CreateAccountForm from 'Components/CreateAccountForm/CreateAccountForm';
 import SearchPage from 'Components/SearchPage/SearchPage';
+
 function App() {
 const [currentUser, setCurrentUser] = useState<CurrentUser | undefined>(undefined);
   // on load => Get user (entire object)
@@ -17,6 +18,17 @@ const [currentUser, setCurrentUser] = useState<CurrentUser | undefined>(undefine
       setCurrentUser(data.data);
     })
   }, [])
+
+
+  const createNewUser = (newUserData: NewUserData) => {
+    postNewUser(newUserData)
+    .then(data => {
+      console.log("posted user", data)
+      setCurrentUser([...currentUser, data])
+    })
+    .catch(error => console.log(error))
+  }
+
   return (
     <>
       <main>
@@ -25,7 +37,7 @@ const [currentUser, setCurrentUser] = useState<CurrentUser | undefined>(undefine
           {!currentUser ? (
             <Route path="/" element={<p>Loading...</p>} />
           ) : (
-            <Route path="/" element={<CreateAccountForm />} />
+            <Route path="/" element={<CreateAccountForm createNewUser={createNewUser}/>} />
           )}
           <Route path="/dashboard" element={<Profile currentUser={currentUser} />}/>
           <Route path="/search" element={<SearchPage currentUser={currentUser} />}/>
