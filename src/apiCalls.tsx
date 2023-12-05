ab43938abc0dc3f82999035fcbdee3c4ef999f7e
Marisa Wyatt <risawyatt22@gmail.com>
2023-12-04 19:54:21 -0700

    feat: Add api call for meetings get

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -15,3 +15,17 @@ export const getSingleUser = () => {
       console.log('res', response.json)
       return response.json()});
 };
+
+export const getMeetingsByUser = (userId: Number) => {
+  return fetch(`https://52d193d3-3f59-4c9e-9d04-d920641190d0.mock.pstmn.io/api/v1/meetings?user_id=${userId}`)
+  .then((res) => {
+    if (res.ok) {
+      return res;
+    } else {
+      throw new Error(
+        `${res.status} ${res.statusText}. Something went wrong, Meetings not found.`
+      );
+    }
+  })
+  .then((response) => response.json());
+}
