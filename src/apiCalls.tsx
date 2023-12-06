15da0c29fa9b603732100db9490eebc26e77b0c2
Jen Ziel <jenziel400@gmail.com>
2023-12-05 19:30:22 -0500

    comment out mock url variable

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,5 +1,5 @@
 import { NewUserData } from 'types';
-const BaseURLMock = 'https://52d193d3-3f59-4c9e-9d04-d920641190d0.mock.pstmn.io'
+// const BaseURLMock = 'https://52d193d3-3f59-4c9e-9d04-d920641190d0.mock.pstmn.io'
 const BaseURLReal = 'https://be-experience-exchange-b6ae91ef3c0f.herokuapp.com/'
 export const getSingleUser = () => {
   return fetch(
@@ -19,8 +19,7 @@ export const getSingleUser = () => {
 
 export const getSearchResults = (query: string) => {
   return fetch(
-    `${BaseURLReal}/api/v1/searched_skills?query=${query}`
-  )
+    `${BaseURLReal}/api/v1/searched_skills?query=${query}` )
     .then((res) => {
       console.log('res1', res)
       if (res.ok) {
