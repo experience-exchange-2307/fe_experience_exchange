128aec203763a1ce20b1c58f1971afcff9c43a18
Jen Ziel <jenziel400@gmail.com>
2023-12-05 19:21:52 -0500

    update base url for actual server

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,5 +1,5 @@
 const BaseURLMock = "https://52d193d3-3f59-4c9e-9d04-d920641190d0.mock.pstmn.io"
-
+const BaseURLReal ="https://be-experience-exchange-b6ae91ef3c0f.herokuapp.com"
 export const getSingleUser = () => {
   return fetch(
     `${BaseURLMock}/api/v1/users/1`
@@ -19,9 +19,9 @@ export const getSingleUser = () => {
 };
 
 export const getSearchResults = (query: string, remoteQuery:string) => {
-  console.log(`${BaseURLMock}/api/v1/search_skills?query=${query}${remoteQuery}`)
+  console.log(`${BaseURLReal}/api/v1/search_skills?query=${query}${remoteQuery}`)
   return fetch(
-    `${BaseURLMock}/api/v1/search_skills?query=${query}${remoteQuery}`
+    `${BaseURLReal}/api/v1/search_skills?query=${query}${remoteQuery}`
 
   )
     .then((res) => {
