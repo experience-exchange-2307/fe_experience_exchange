29a3dfbcb124e9ca810952643a8801a1db76e8a0
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-12-09 10:09:41 -0500

    Rename handle input functions

--- src/Components/Skill/Skill.tsx
+++ src/Components/Skill/Skill.tsx
@@ -1,46 +1,34 @@
 import "./Skill.css";
 import React, { useState, useEffect } from "react";
+import { getSingleUser, postSkills } from "apiCalls";
 import ProgressBar from "@ramonak/react-progress-bar";
-import { postSkills } from "apiCalls";
 import { UserSkill } from "types";
 import { useParams } from "react-router-dom";
-import { stringify } from "querystring";
-import { getSingleUser } from "apiCalls";
-import { CurrentUser } from "types";
-
-// GET request to tget skills
 
 function SkillForm() {
-  const { id } = useParams()
   const [userSkills, setUserSkills] = useState<UserSkill[]>([])
   const [currentTag, setCurrentTag] = useState('')
   const [proficiency, setProficiency] = useState(0)
   const [alert, setAlert] = useState("");
-
-  // const currentUserID = id
+  const { id } = useParams<{id: string | undefined}>()
+  const userId = id ? parseInt(id, 10) : undefined;
 
   useEffect(() => {
-        getSingleUser(14)
-        .then((data) => {
-          console.log("data", data.data);
-          setUserSkills(data.data.attributes.skills);
-        })
+    if(userId !== undefined && !isNaN(userId)) {
+
+      getSingleUser(userId)
+      .then((data) => {
+        console.log("data", data.data);
+        setUserSkills(data.data.attributes.skills);
+      })
+    }
   }, [])
 
-  const setInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSkillInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCurrentTag(e.target.value)
   };
 
   const submitNewSkill = () => {
-
-// skillForm and SkillList component 
-// then GET skills when you need it or GET current user and only take skills from it
-// only get the skills, 
-
-// currentUser.id === id
-
-// rename to Skills, contains skills form and skills list
-
     if (currentTag.trim() !== '') {
       // check if skill already exists
       if (!userSkills.some((skill) => skill.name.toLowerCase() === currentTag.trim().toLowerCase())) {
@@ -53,13 +41,14 @@ function SkillForm() {
         const combinedSkills = [...userSkills, newSkill]
 
         postSkills(id, combinedSkills)
-        .then((data) => {
-                console.log('skills posted successfully:', data.data.attributes.skills)
-                setUserSkills(data.data.attributes.skills)
-              })
-              .catch((error) => {
-                console.error('error posting skills:', error)
-              });
+          .then((data) => {
+          console.log('skills posted successfully:', data.data.attributes.skills)
+          setUserSkills(data.data.attributes.skills)
+        })
+        .catch((error) => {
+          console.error('error posting skills:', error)
+        })
+        setAlert("")
         setCurrentTag('')
         setProficiency(0)
       } else {
@@ -68,7 +57,7 @@ function SkillForm() {
     }
   };
   
-  const handleProficiencyChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleProficiencyInput = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedProficiency = parseInt(e.target.value, 10)
     setProficiency(selectedProficiency)
   };
@@ -84,12 +73,12 @@ function SkillForm() {
         <input
           type="text"
           value={currentTag}
-          onChange={setInputValue}
+          onChange={handleSkillInput}
           placeholder="Type skill name"
         />
 
-        <label htmlFor="proficiency">Select Proficiency Level:</label>
-        <select name="proficiency" id="proficiency" onChange={handleProficiencyChange}>
+        <label htmlFor="proficiency">Proficiency:</label>
+        <select name="proficiency" id="proficiency" onChange={handleProficiencyInput}>
           <option value="0">0</option>
           <option value="1">1</option>
           <option value="2">2</option>
