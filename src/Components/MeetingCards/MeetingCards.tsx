8c76f1ce4988810ac5ceb20fca698936ccf74565
Jen Ziel <jenziel400@gmail.com>
2023-12-09 15:05:14 -0500

    format the date

--- src/Components/MeetingCards/MeetingCards.tsx
+++ src/Components/MeetingCards/MeetingCards.tsx
@@ -18,42 +18,57 @@ const handleReject = (meetingId: number) => {
 };
 
 const MeetingCards: React.FC<MeetingCardsProps> = ({ meetings }) => {
+  function trimLeadingZero(time: string): string {
+    if (time.length > 0 && time.charAt(0) === "0") {
+      return time.substring(1);
+    }
+    return time;
+  }
   return (
     <div>
       {meetings.map((meeting) => {
         const formattedDate = dayjs(meeting.attributes.date).format("MMM. D");
+        const formattedStartTime = trimLeadingZero(
+          meeting.attributes.start_time
+        );
+        const formattedEndTime = trimLeadingZero(meeting.attributes.end_time);
+
         return (
-          <div key={meeting.id} className="meeting-card">
+          <div key={meeting.id} className='meeting-card'>
             {!meeting.attributes.is_accepted ? (
               <div className='meeting-card-request meeting-card-content'>
                 <h3>REQUEST</h3>
                 <p>{formattedDate}</p>
                 <p>
-                  {meeting.attributes.start_time} -{" "}
-                  {meeting.attributes.end_time}
+                  {formattedStartTime} - {formattedEndTime}
                 </p>
                 {/* do fetch for partner name? or ask BE if they can add a name to meeting data */}
                 <p>Partner ID: {meeting.attributes.partner_id}</p>
                 <div className='meeting-card-btn-container'>
-            <button onClick={() => handleAccept(meeting.id)} className='meeting-card-btn'>Accept</button>
-                <button onClick={() => handleReject(meeting.id)} className='meeting-card-btn'>
-                  Decline
-                </button>
+                  <button
+                    onClick={() => handleAccept(meeting.id)}
+                    className='meeting-card-btn'
+                  >
+                    Accept
+                  </button>
+                  <button
+                    onClick={() => handleReject(meeting.id)}
+                    className='meeting-card-btn'
+                  >
+                    Decline
+                  </button>
                 </div>
               </div>
             ) : (
               <div className='meeting-card-content'>
                 <p>{formattedDate}</p>
                 <p>
-                   {meeting.attributes.start_time} -{" "}
-                  {meeting.attributes.end_time}
+                  {formattedStartTime} - {formattedEndTime}
                 </p>
                 {/* do fetch for partner name? or ask BE if they can add a name to meeting data */}
                 <p>Partner ID: {meeting.attributes.partner_id}</p>
-
               </div>
             )}
-        
           </div>
         );
       })}
