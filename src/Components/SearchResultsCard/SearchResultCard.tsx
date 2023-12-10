2293a9b32c0454232374e42f94bf17d7f928458d
Jen Ziel <jenziel400@gmail.com>
2023-12-09 19:44:47 -0500

    style the search page

--- src/Components/SearchResultsCard/SearchResultCard.tsx
+++ src/Components/SearchResultsCard/SearchResultCard.tsx
@@ -20,13 +20,19 @@ function SearchResultCard({
   id,
 }: SearchResultCardProps) {
   const lastSkillIndex = skills.length - 1;
-
+  const linkStyle = {
+    color: "inherit",
+    textDecoration: "none",
+   
+  };
   return (
-    <Link to={`/dashboard/${id}`}>
+    <Link to={`/dashboard/${id}`} style={linkStyle}>
       <div className='result-card' key={id}>
+        <div className='result-card-left'>
+
         <p className='result-card-title'>{`${first_name} ${last_name}`}</p>
         <div className='result-card-skills-container'>
-          <p className='result-card-title'>Skills:</p>
+          <p className='result-card-title2'>Skills:</p>
 
           {skills.map((skill, index) => (
             <p key={index} className='result-card-skills'>
@@ -37,10 +43,11 @@ function SearchResultCard({
         </div>
 
         <div>
+        </div>
+        </div>
           <p className='result-card-distance'>
             {is_remote ? "Remote" : `${distance} miles away`}
           </p>
-        </div>
       </div>
     </Link>
   );
--- src/Components/Skill/Skill.tsx
+++ src/Components/Skill/Skill.tsx
@@ -6,110 +6,129 @@ import { UserSkill } from "types";
 import { useParams } from "react-router-dom";
 
 function SkillForm() {
-  const [userSkills, setUserSkills] = useState<UserSkill[]>([])
-  const [currentTag, setCurrentTag] = useState('')
-  const [proficiency, setProficiency] = useState(0)
+  const [userSkills, setUserSkills] = useState<UserSkill[]>([]);
+  const [currentTag, setCurrentTag] = useState("");
+  const [proficiency, setProficiency] = useState(0);
   const [alert, setAlert] = useState("");
-  const { id } = useParams<{id: string | undefined}>()
+  const { id } = useParams<{ id: string | undefined }>();
   const userId = id ? parseInt(id, 10) : undefined;
 
   useEffect(() => {
-    if(userId !== undefined && !isNaN(userId)) {
-
-      getSingleUser(userId)
-      .then((data) => {
-        console.log("data", data.data)
-        setUserSkills(data.data.attributes.skills)
-      })
+    if (userId !== undefined && !isNaN(userId)) {
+      getSingleUser(userId).then((data) => {
+        console.log("data", data.data);
+        setUserSkills(data.data.attributes.skills);
+      });
     }
-        // eslint-disable-next-line
-  }, [])
+    // eslint-disable-next-line
+  }, []);
 
   const handleSkillInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCurrentTag(e.target.value)
+    setCurrentTag(e.target.value);
   };
 
   const submitNewSkill = () => {
-    if (currentTag.trim() !== '') {
+    if (currentTag.trim() !== "") {
       // check if skill already exists
-      if (!userSkills.some((skill) => skill.name.toLowerCase() === currentTag.trim().toLowerCase())) {
-
+      if (
+        !userSkills.some(
+          (skill) =>
+            skill.name.toLowerCase() === currentTag.trim().toLowerCase()
+        )
+      ) {
         const newSkill: UserSkill = {
           name: currentTag.trim(),
           proficiency: Number(proficiency),
         };
 
-        const combinedSkills = [...userSkills, newSkill]
+        const combinedSkills = [...userSkills, newSkill];
 
         postSkills(id, combinedSkills)
           .then((data) => {
-          console.log('skills posted successfully:', data.data.attributes.skills)
-          setUserSkills(data.data.attributes.skills)
-        })
-        .catch((error) => {
-          console.error('error posting skills:', error)
-        })
-        setAlert("")
-        setCurrentTag('')
-        setProficiency(0)
+            console.log(
+              "skills posted successfully:",
+              data.data.attributes.skills
+            );
+            setUserSkills(data.data.attributes.skills);
+          })
+          .catch((error) => {
+            console.error("error posting skills:", error);
+          });
+        setAlert("");
+        setCurrentTag("");
+        setProficiency(0);
       } else {
-        setAlert('Skill already exists!')
+        setAlert("Skill already exists!");
       }
     }
   };
-  
+
   const handleProficiencyInput = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedProficiency = parseInt(e.target.value, 10)
-    setProficiency(selectedProficiency)
+    const selectedProficiency = parseInt(e.target.value, 10);
+    setProficiency(selectedProficiency);
   };
 
   const handleTagRemove = (skillToRemove: UserSkill) => {
-    const updatedSkills = userSkills.filter((skill) => skill !== skillToRemove)
-    setUserSkills(updatedSkills)
+    const updatedSkills = userSkills.filter((skill) => skill !== skillToRemove);
+    setUserSkills(updatedSkills);
   };
 
   return (
     <div>
-      <div>
+      <div className='skill-form-wrapper'>
         <input
-          type="text"
+          type='text'
           value={currentTag}
           onChange={handleSkillInput}
-          placeholder="Type skill name"
-          className="skill-input"
+          placeholder='Type skill name'
+          className='skill-input'
         />
-
-        <label htmlFor="proficiency" className='proficiency-label'>Proficiency:</label>
-        <select name="proficiency" id="proficiency" onChange={handleProficiencyInput} className='proficiency-input'>
-          <option value="0">0</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-        </select>
-        <p className="alert-message">{alert}</p>
-        <button onClick={submitNewSkill} className='add-skill-btn'>Add Skill</button>
+        <div>
+          <label htmlFor='proficiency' className='proficiency-label'>
+            Proficiency:
+          </label>
+          <select
+            name='proficiency'
+            id='proficiency'
+            onChange={handleProficiencyInput}
+            className='proficiency-input'
+          >
+            <option value='0'>0</option>
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+            <option value='5'>5</option>
+          </select>
+          <p className='alert-message'>{alert}</p>
+          <button onClick={submitNewSkill} className='add-skill-btn'>
+            Add Skill
+          </button>
+        </div>
       </div>
 
       {userSkills.map((skill, index) => (
-        <div key={index}>
-          <div className="skill-list-container">
-            <p className='skill-name' 
-              key={`skill-name-${index}`}>
+        <div key={index} className="skill-list">
+          <div className='skill-list-container'>
+            <p className='skill-name' key={`skill-name-${index}`}>
               {skill.name}
-               <button className="tag-removal" onClick={() => handleTagRemove(skill)}>x</button>
+              <button
+                className='tag-removal'
+                onClick={() => handleTagRemove(skill)}
+              >
+                x
+              </button>
             </p>
           </div>
           <ProgressBar
             key={`progress-${index}`}
             className='progress-bar'
             completed={skill.proficiency}
-            bgColor="#f76a1e"
-            height="7px"
+            bgColor='#f76a1e'
+            height='7px'
             isLabelVisible={false}
-            baseBgColor="#cecece"
-            labelColor="#f76a1e"
+            baseBgColor='#ffffff'
+            labelColor='#f76a1e'
             animateOnRender
             maxCompleted={5}
           />
