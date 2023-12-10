07c7a005dc493d0a49a7d8f6f91f74acf2a497a3
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-12-10 14:44:29 -0500

    Add ErrorPageProps interface

--- src/Components/ErrorPage/ErrorPage.tsx
+++ src/Components/ErrorPage/ErrorPage.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
-function ErrorPage() {
+interface ErrorPageProps {
+  serverError?: string;
+  message?: string;
+}
+
+function ErrorPage({serverError, message}: ErrorPageProps) {
   return (
-    <div>
+    <section>
+       {serverError && <p>{serverError}</p>}
+       <h2>{message}</h2>
       <h2>There was an error</h2>
-    </div>
+    </section>
   )
 }
 
