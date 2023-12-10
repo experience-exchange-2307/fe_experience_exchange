d197d7afc8b0f58e5f4a9e8a5607d78b9b0aae59
Jen Ziel <jenziel400@gmail.com>
2023-12-10 16:00:00 -0500

    Add tests for dynamic search route and the button to return to search results.

--- cypress/e2e/searchPage.cy.ts
+++ cypress/e2e/searchPage.cy.ts
@@ -7,7 +7,23 @@ describe("a user should be able to search by keyword", () => {
         status: 200,
         fixture: "currentUser",
       }
-    )
+    ).as("GetCurrentUser")
+    cy.intercept(
+      "GET",
+      "https://be-experience-exchange-b6ae91ef3c0f.herokuapp.com/api/v1/users/15",
+      {
+        status: 200,
+        fixture: "TylerInfo",
+      }
+    ).as("GetTyler");
+    cy.intercept(
+      "GET",
+      "https://be-experience-exchange-b6ae91ef3c0f.herokuapp.com/api/v1/users/14/meetings",
+      {
+        status: 200,
+        fixture: "MyMeetings",
+      }
+    ).as("GetMyMeetings");
     cy.intercept(
       "GET",
       "https://be-experience-exchange-b6ae91ef3c0f.herokuapp.com/api/v1/search_skills?query=piano&user_id=14",
@@ -15,37 +31,78 @@ describe("a user should be able to search by keyword", () => {
         status: 200,
         fixture: "searchResults",
       }
-    ).as('searchPiano').visit("http://localhost:3000/search");
+    )
+      .as("searchPiano")
+      .visit("http://localhost:3000/search");
   });
 
   it("should have a controlled search form that displays results when submit button is clicked", () => {
-    cy.get('.search-title').contains('p', 'Find people near you')
-    cy.get('.search-input').type('piano')
-    cy.get('.search-submit-btn').click()
-    .wait('@searchPiano')
-    cy.get('.search-results-qty').contains('h1', 'Showing 2 Results for piano')
-    cy.get('.result-card').should('have.length', 2)
-    .get('.result-card').first().contains('p', 'Tyler Blackmon')
-    .get('.result-card').first().contains('p', 'Skills:')
-    .get('.result-card').first().contains('p', 'Ruby')
-    .get('.result-card').first().contains('p', 'Piano')
-    .get('.result-card').first().contains('p', '1 mi.')
-    .get('.result-card').last().contains('p', 'Jen Ziel')
-    .get('.result-card').last().contains('p', 'Skills:')
-    .get('.result-card').last().contains('p', 'piano')
-    .get('.result-card').last().contains('p', 'knitting')
-    .get('.result-card').last().contains('p', 'juggling')
-    .get('.result-card').last().contains('p', 'Remote')
-    cy.get('#remote').click()
-    cy.get('.result-card').should('have.length', 1)
-    .get('.result-card').last().contains('p', 'Jen Ziel')
-    .get('.result-card').last().contains('p', 'Skills:')
-    .get('.result-card').last().contains('p', 'piano')
-    .get('.result-card').last().contains('p', 'knitting')
-    .get('.result-card').last().contains('p', 'juggling')
-    cy.get('.result-card').first().contains('p', 'Remote')
-    cy.get('#remote').click()
-    cy.get('.result-card').should('have.length', 2)
+    cy.get(".search-title").contains("p", "Find people near you");
+    cy.get(".search-input").type("piano");
+    cy.get(".search-submit-btn").click().wait("@searchPiano");
+    cy.url().should("include", "/search/piano");
+    cy.get(".search-results-qty").contains("h1", "Showing 2 Results for piano");
+    cy.get(".result-card")
+      .should("have.length", 2)
+      .get(".result-card")
+      .first()
+      .contains("p", "Tyler Blackmon")
+      .get(".result-card")
+      .first()
+      .contains("p", "Skills:")
+      .get(".result-card")
+      .first()
+      .contains("p", "Ruby")
+      .get(".result-card")
+      .first()
+      .contains("p", "Piano")
+      .get(".result-card")
+      .first()
+      .contains("p", "1 mi.")
+      .get(".result-card")
+      .last()
+      .contains("p", "Jen Ziel")
+      .get(".result-card")
+      .last()
+      .contains("p", "Skills:")
+      .get(".result-card")
+      .last()
+      .contains("p", "piano")
+      .get(".result-card")
+      .last()
+      .contains("p", "knitting")
+      .get(".result-card")
+      .last()
+      .contains("p", "juggling")
+      .get(".result-card")
+      .last()
+      .contains("p", "Remote");
+    cy.get("#remote").click();
+    cy.url().should("include", "/search/piano");
+    cy.get(".search-results-qty").contains("h1", "Showing 1 Results for piano");
+    cy.get(".result-card")
+      .should("have.length", 1)
+      .get(".result-card")
+      .last()
+      .contains("p", "Jen Ziel")
+      .get(".result-card")
+      .last()
+      .contains("p", "Skills:")
+      .get(".result-card")
+      .last()
+      .contains("p", "piano")
+      .get(".result-card")
+      .last()
+      .contains("p", "knitting")
+      .get(".result-card")
+      .last()
+      .contains("p", "juggling");
+    cy.get(".result-card").first().contains("p", "Remote");
+    cy.get("#remote").click();
+    cy.get(".result-card").should("have.length", 2);
+    cy.get(".search-results-qty").contains("h1", "Showing 2 Results for piano");
+    cy.get(".result-card").first().click();
+    cy.url().should("include", "/dashboard/15");
+    cy.get('.back-to-search-btn').should('be.visible')
   });
-
 });
