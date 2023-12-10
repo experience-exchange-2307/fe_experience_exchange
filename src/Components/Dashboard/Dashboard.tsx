e784472395f8e4579a4ca79a4a2cb9dfe59adfa1
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-12-10 14:20:02 -0500

    Add setServerInterface. Add to getSingleUser and getMeetingsByUser calls. Pass to Profile and RequestMeeting Form

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -9,10 +9,11 @@ import ProfileHeader from '../ProfileHeader/ProfileHeader'
 import './Dashboard.css'
 
 interface CurrentUserProps {
-  currentUser: CurrentUser;
+  currentUser: CurrentUser; 
+  setServerError: (error: string) => void;
 }
 
-function Dashboard({ currentUser }: CurrentUserProps) {
+function Dashboard({ currentUser, setServerError }: CurrentUserProps) {
   const { id } = useParams();
   const [userMeetings, setUserMeetings] = useState([]);
   const [isCurrentUser, setIsCurrentUser] = useState<Boolean>(true);
@@ -28,6 +29,7 @@ function Dashboard({ currentUser }: CurrentUserProps) {
         })
         .catch((error) => {
           console.error("Error fetching user data:", error);
+          setServerError(error)
         });
     }
 
@@ -37,6 +39,7 @@ function Dashboard({ currentUser }: CurrentUserProps) {
       })
       .catch((error) => {
         console.error("Error fetching meetings:", error);
+        setServerError(error)
       });
 
     setIsCurrentUser(isCurrentUserDashboard);
@@ -50,15 +53,15 @@ function Dashboard({ currentUser }: CurrentUserProps) {
       {!isCurrentUser && dashboardData ? (
         <div className="other-user-dash">
           <ProfileHeader currentUser={dashboardData} />
-          <Profile currentUser={dashboardData} />
-          <RequestMeetingForm currentUserId={currentUser.id} />
+          <Profile currentUser={dashboardData} setServerError={setServerError}/>
+          <RequestMeetingForm currentUserId={currentUser.id} setServerError={setServerError}/>
         </div>
       ) : null}
 
       {isCurrentUser && (
         <div className="current-user-dash">
           <ProfileHeader currentUser={currentUser} />
-          <Profile currentUser={currentUser} />
+          <Profile currentUser={currentUser} setServerError={setServerError}/>
           <MeetingsContainer meetings={userMeetings} />
         </div>
       )}
