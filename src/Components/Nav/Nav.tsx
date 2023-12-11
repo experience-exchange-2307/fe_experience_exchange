22994b6c1962c75033e65ca4e41936b07f1696e3
Jen Ziel <jenziel400@gmail.com>
2023-12-11 11:18:06 -0500

    add nav link for create account form

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -24,7 +24,28 @@ function Nav({ currentUser }: CurrentUserProps) {
       <h1 className='logo-text-h1'>Exchange</h1>
       </div>
       </div>
-      <nav className='nav-links-container'>
+      <nav className='nav-links-container'>    <NavLink
+          to={`/`}
+          className='nav-bar-link'
+          style={linkStyle}
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='w-6 h-6'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z'
+            />
+          </svg>
+
+          <p className='nav-bar-text'>Create Account</p>
+        </NavLink>
         <NavLink
           to={`/dashboard/${currentUser?.id}`}
           className='nav-bar-link'
