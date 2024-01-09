71b7bab94dd39ecb8c53561e8518d9df113cce2c
Marisa Wyatt <risawyatt22@gmail.com>
2024-01-09 14:37:49 -0700

    feat: Add pending conditional render

--- src/Components/MeetingCards/MeetingCards.tsx
+++ src/Components/MeetingCards/MeetingCards.tsx
@@ -8,7 +8,12 @@ interface MeetingCardsProps {
   onReject?: (meetingId: number) => void;
 }
 
-const MeetingCards: React.FC<MeetingCardsProps> = ({ meetings, onAccept, onReject }) => {
+const MeetingCards: React.FC<MeetingCardsProps> = ({
+  meetings,
+  onAccept,
+  onReject,
+}) => {
+  console.log(meetings);
 
   function trimLeadingZero(time: string): string {
     if (time.length > 0 && time.charAt(0) === "0") {
@@ -17,47 +22,61 @@ const MeetingCards: React.FC<MeetingCardsProps> = ({ meetings, onAccept, onRejec
     return time;
   }
   return (
-    <div  className='meeting-card-wrapper'>
+    <div className="meeting-card-wrapper">
       {meetings.map((meeting) => {
         const formattedDate = dayjs(meeting.attributes.date).format("MMM. D");
         const formattedStartTime = trimLeadingZero(
           meeting.attributes.start_time
         );
         const formattedEndTime = trimLeadingZero(meeting.attributes.end_time);
+        const isPending =
+          !meeting.attributes.is_accepted && !meeting.attributes.is_host;
 
         return (
-          <div key={meeting.id} className='meeting-card'>
+          // Want to make Partner: {meeting.attributes.partner_name} a link to partner's
+          <div key={meeting.id} className="meeting-card">
             {!meeting.attributes.is_accepted ? (
-              <div className='meeting-card-request meeting-card-content'>
-                <h3>REQUEST</h3>
-                <p>{formattedDate}</p>
-                <p>
-                  {formattedStartTime} - {formattedEndTime}
-                </p>
-                <p>Partner ID: {meeting.attributes.partner_id}</p>
-                <div className='meeting-card-btn-container'>
-                  <button
-                    onClick={() => onAccept?.(meeting.id)}
-                    className='meeting-card-btn'
-                  >
-                    Accept
-                  </button>
-                  <button
-                    onClick={() => onReject?.(meeting.id)}
-                    className='meeting-card-btn'
-                  >
-                    Decline
-                  </button>
+              isPending ? (
+                <div className="meeting-card-pending meeting-card-content">
+                  <h3>PENDING</h3>
+                  <p>{formattedDate}</p>
+                  <p>
+                    {formattedStartTime} - {formattedEndTime}
+                  </p>
+                  <p>Partner: {meeting.attributes.partner_name}</p>
+                  <p>Meeting pending, waiting on partner to respond</p>
                 </div>
-              </div>
+              ) : (
+                <div className="meeting-card-request meeting-card-content">
+                  <h3>REQUEST</h3>
+                  <p>{formattedDate}</p>
+                  <p>
+                    {formattedStartTime} - {formattedEndTime}
+                  </p>
+                  <p>Partner: {meeting.attributes.partner_name}</p>
+                  <div className="meeting-card-btn-container">
+                    <button
+                      onClick={() => onAccept?.(meeting.id)}
+                      className="meeting-card-btn"
+                    >
+                      Accept
+                    </button>
+                    <button
+                      onClick={() => onReject?.(meeting.id)}
+                      className="meeting-card-btn"
+                    >
+                      Decline
+                    </button>
+                  </div>
+                </div>
+              )
             ) : (
-              <div className='meeting-card-content'>
+              <div className="meeting-card-content">
                 <p>{formattedDate}</p>
                 <p>
                   {formattedStartTime} - {formattedEndTime}
                 </p>
-                {/* do fetch for partner name? or ask BE if they can add a name to meeting data */}
-                <p>Partner ID: {meeting.attributes.partner_id}</p>
+                <p>Partner: {meeting.attributes.partner_name}</p>
               </div>
             )}
           </div>
