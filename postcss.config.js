df587f87e155f16c67ae5ffde9af78c643ae3cc0
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-01-09 17:15:35 -0500

    Update postcss module exports

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,8 @@ module.exports = {
     'postcss-import': {},
     'tailwindcss/nesting': 'postcss-nesting',
     tailwindcss: {},
-    autoprefixer: {},
+    'postcss-preset-env': {
+      features: { 'nesting-rules': false },
+    },
   }
 }
