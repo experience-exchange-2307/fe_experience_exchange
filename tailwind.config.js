783e37e4e400f54f316a94376784fa1bed645655
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-01-09 13:57:26 -0500

    Install daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+    
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
 
