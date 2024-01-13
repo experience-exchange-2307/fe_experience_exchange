4f53beb09d16c3db6fc891b1aad54481390c493b
Jen Ziel <jenziel400@gmail.com>
2024-01-12 20:24:35 -0500

    Format cypress files

--- cypress/e2e/pageLoad.cy.ts
+++ cypress/e2e/pageLoad.cy.ts
@@ -80,33 +80,40 @@ describe("Page Load", () => {
     cy.url().should("include", "/dashboard/14");
     cy.get(".current-user-dash .meetings-container").should("exist");
     cy.get(".profile-name").should("have.text", "Jackson Williams");
-    cy.get('.profile-about').should('have.text', 'About me');
-    cy.get('.profile-about-text').should('have.text', `Hey there! I'm Jackson, and life's a garden – dig it! 🌱 When I'm not tending to my plants, you can find me crafting something beautiful with wood or tickling the ivories on the piano. 🪚🎶`);
-    cy.get('.profile-location').should('have.text', 'Location');
-    cy.get('.profile-location-text').should('have.text', 'Brooklyn, New York');
-    cy.get('.profile-email').should('have.text', 'Email');
-    cy.get('.profile-email-text').should('have.text', 'jwilliams@gmail.com');
+    cy.get(".profile-about").should("have.text", "About me");
+    cy.get(".profile-about-text").should(
+      "have.text",
+      `Hey there! I'm Jackson, and life's a garden – dig it! 🌱 When I'm not tending to my plants, you can find me crafting something beautiful with wood or tickling the ivories on the piano. 🪚🎶`
+    );
+    cy.get(".profile-location").should("have.text", "Location");
+    cy.get(".profile-location-text").should("have.text", "Brooklyn, New York");
+    cy.get(".profile-email").should("have.text", "Email");
+    cy.get(".profile-email-text").should("have.text", "jwilliams@gmail.com");
     // skills component
-    cy.get('.skills-section .skill-list').should('have.length', 3);
-    cy.get('.tag-removal').should('have.length', 3);
-    cy.get('.skill-input').should('have.attr', 'type', 'text');
-    cy.get('.skill-input').should('have.attr', 'placeholder', 'Type skill name');
-    cy.get('.proficiency-input').should('have.attr', 'name', 'proficiency');
-    cy.get('.proficiency-input').should('have.value', '0');
-    cy.get('.add-skill-btn').should('exist');
-    cy.get('.add-skill-btn').should('have.text', 'Add Skill');
+    cy.get(".skills-section .skill-list").should("have.length", 3);
+    cy.get(".tag-removal").should("have.length", 3);
+    cy.get(".skill-input").should("have.attr", "type", "text");
+    cy.get(".skill-input").should(
+      "have.attr",
+      "placeholder",
+      "Type skill name"
+    );
+    cy.get(".proficiency-input").should("have.attr", "name", "proficiency");
+    cy.get(".proficiency-input").should("have.value", "0");
+    cy.get(".add-skill-btn").should("exist");
+    cy.get(".add-skill-btn").should("have.text", "Add Skill");
 
     const userSkills = [
-      { name: 'Ruby', proficiency: 5 },
-      { name: 'Rails', proficiency: 5 },
-      { name: 'knitting', proficiency: 1 },
+      { name: "Ruby", proficiency: 5 },
+      { name: "Rails", proficiency: 5 },
+      { name: "knitting", proficiency: 1 },
     ];
 
     userSkills.forEach((skill, index) => {
       cy.get(`.skills-section .skill-list:eq(${index}) .skill-text`)
-        .invoke('text')
-        .invoke('trim')
-        .should('eq', skill.name.trim());
+        .invoke("text")
+        .invoke("trim")
+        .should("eq", skill.name.trim());
     });
     // Meetings component
     cy.get(".meetings-container").should("exist");
@@ -114,6 +121,6 @@ describe("Page Load", () => {
     cy.get(".meeting-card").should("have.length", 2);
     cy.get(".meetings-toggle").click();
     cy.get(".meetings-title").should("have.text", "My Meeting Requests");
-    cy.get(".meeting-card-request").should("have.length", 2); 
+    cy.get(".meeting-card-request").should("have.length", 2);
   });
 });
